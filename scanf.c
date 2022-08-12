ff620a44177b107398259f220ace41edf80bb5be
Angelawaaa <110403543+Angelawaaa@users.noreply.github.com>
2022-08-13 01:17:37 +0800

    Create scanf.c

---
+++ scanf.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int a = 2022;
+int main()
+{
+	int a = 0;
+	int b = 0;
+	scanf("%d %d",a,b);
+	int sum = 0;
+	printf("sum=%d\n", sum);
+	return 0;
+}
